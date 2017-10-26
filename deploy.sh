28aae6eb0bbf9f5a4e5551c149291fc85ee48a19
Student <jeff.kandel@gmail.com>
2017-10-26 17:28:29 -0400

    creating github page

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo 'DEPLOY TO HEROKU!!!'
+
+git checkout -b deploy
+
+git pull heroku master
+elm-app build
+git add -f build/static/*
+git commit --allow-empty -m 'Deploying'
+git push heroku deploy:master
+
+git checkout master
+git branch -D deploy
