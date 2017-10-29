4bc1a3dc4269aebd6dee3767330657979f820322
Student <jeff.kandel@gmail.com>
2017-10-29 01:17:02 -0400

    refactor: switch serve for express to allow clothing api

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 8080
+const app = express()
+module.exports = app
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
+
+  // body parsing middleware
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'build')))
+
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+  })
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+}
+
+createApp()
+startListening()
+
