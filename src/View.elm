cd413b50035ee37578a93e1f787494bade1c108a
Student <jeff.kandel@gmail.com>
2017-10-29 23:39:47 -0400

    feature: clarify UI with labels

--- src/View.elm
+++ src/View.elm
@@ -27,58 +27,78 @@ view model =
                     tachyons.css
                     , text "Run Location: Central Park"
                 ]
-                , div [classes [flex, pa4]] [
-                        Html.a [ classes [(if model.gender == "male" then "bg-black white" else ""), w_50, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1] , onClick <| SetGender "male"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Male"
+                , div [classes [flex, flex_column, pa4]] [
+                        Html.label [classes [tl]][
+                            Html.span [ classes [f6, dark_gray]] [
+                                text "My clothes style is..."
                             ]
                         ]
-                        , Html.a [ classes [(if model.gender == "female" then "bg-black white" else ""), w_50, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetGender "female"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Female"
+                        ,div [classes [flex]] [
+                            Html.a [ classes [(if model.gender == "male" then "bg-black white" else ""), w_50, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1] , onClick <| SetGender "male"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Male"
+                                ]
                             ]
-                        ]
-                ]
-                , div [classes [flex, pa4]] [
-                        Html.a [ classes [(if model.feel == "cool" then "bg-black white" else ""), w_33, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1] , onClick <| SetFeel "cool"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Cool"
-                            ]
-                        ]
-                        , Html.a [ classes [(if model.feel == "in between" then "bg-black white" else ""), w_33, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetFeel "in between"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Medium"
-                            ]
-                        ]
-                        , Html.a [ classes [(if model.feel == "warm" then "bg-black white" else ""), w_33, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetFeel "warm"] [
-                            Html.span [classes [center, pl1]] [
-                                text "Warm"
+                            , Html.a [ classes [(if model.gender == "female" then "bg-black white" else ""), w_50, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetGender "female"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Female"
+                                ]
                             ]
                         ]
                 ]
-                , div [classes [flex, pa4]] [
-                        Html.a [ classes [(if model.intensity == "easy run" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1] , onClick <| SetIntensity "easy run"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Easy"
+                , div [classes [flex, flex_column, pa4]] [
+                        Html.label [classes [tl]][
+                            Html.span [ classes [f6, dark_gray]] [
+                                text "I want to feel..."
                             ]
                         ]
-                        , Html.a [ classes [(if model.intensity == "long run" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetIntensity "long run"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Long"
+                        ,div [classes [flex]] [
+                            Html.a [ classes [(if model.feel == "cool" then "bg-black white" else ""), w_33, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1] , onClick <| SetFeel "cool"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Cool"
+                                ]
                             ]
-                        ]
-                        , Html.a [ classes [(if model.intensity == "hard workout" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetIntensity "hard workout"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Hard"
+                            , Html.a [ classes [(if model.feel == "in between" then "bg-black white" else ""), w_33, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetFeel "in between"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Medium"
+                                ]
                             ]
-                        ]
-                        , Html.a [ classes [(if model.intensity == "race" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetIntensity "race"] [
-                            Html.span [classes [ center, pl1]] [
-                                text "Race"
+                            , Html.a [ classes [(if model.feel == "warm" then "bg-black white" else ""), w_33, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetFeel "warm"] [
+                                Html.span [classes [center, pl1]] [
+                                    text "Warm"
+                                ]
                             ]
-                        ]
+                    ]
                 ]
-
+                , div [classes [flex, flex_column, pa4]] [
+                        Html.label [classes [tl]][
+                            Html.span [ classes [f6, dark_gray]] [
+                                text "My run will be..."
+                            ]
+                        ]
+                        ,div [classes [flex]] [
+                            Html.a [ classes [(if model.intensity == "easy run" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1] , onClick <| SetIntensity "easy run"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Easy"
+                                ]
+                            ]
+                            , Html.a [ classes [(if model.intensity == "long run" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetIntensity "long run"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Long"
+                                ]
+                            ]
+                            , Html.a [ classes [(if model.intensity == "hard workout" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetIntensity "hard workout"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Hard"
+                                ]
+                            ]
+                            , Html.a [ classes [(if model.intensity == "race" then "bg-black white" else ""), w_25, f5, no_underline, black, bg_animate, hover_bg_black, hover_white, inline_flex, items_center, pa3, ba, border_box, mr1], onClick <| SetIntensity "race"] [
+                                Html.span [classes [ center, pl1]] [
+                                    text "Race"
+                                ]
+                            ]
+                    ]
+                ]
             ]
         ]
         , div [ classes [ w_33_l, bg_moon_gray, items_center, justify_center, pa4, mw7, center, br2_ns, ba, b__black_10]] [
